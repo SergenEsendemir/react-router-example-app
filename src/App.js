a64df838cbd80e85826f024297790f3697183ee9
SergenEsendemir <sergen_35_96@hotmail.com>
2022-01-05 11:31:01 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
+
+import About from "./components/About";
+import Home from "./components/Home";
+import Users from "./components/Users";
+import Error404 from "./components/Error404";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <NavLink activeClassName="active" to="/" exact>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink activeClassName="active" to="/about">
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink activeClassName="active" to="/users">
+                  Users
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/users" component={Users} />
+            <Route path="*" component={Error404} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
